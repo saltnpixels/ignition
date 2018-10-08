543270bb5733986354bd9b8bb1d3fe25852aec80
Eric Greenfield <eric@saltnpixels.com>
2018-10-07 20:34:11 -0400

    added anchor smooth scrolling
    fixed anchor smooth scrolling and added new section scrolling field

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -480,36 +480,39 @@ jQuery(function ($) {
 });
 "use strict";
 
-// Select all links with hashes
-$('a[href*="#"]') // Remove links that don't actually link to anything
-.not('[href="#"]').not('[href="#0"]').click(function (event) {
-  // On-page links
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    var offset = $(this.hash).data('offset'); // Does a scroll target exist?
+jQuery(function ($) {
+  // Select all links with hashes
+  $('a[href*="#"]') // Remove links that don't actually link to anything
+  .not('[href="#"]').not('[href="#0"]').click(function (event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      var offset = $(this).data('anchor-offset');
 
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top - offset
-      }, 1000, function () {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
+      if (!offset) {
+        offset = 0;
+      } // Does a scroll target exist?
 
-        if ($target.is(":focus")) {
-          // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
 
-          $target.focus(); // Set focus again
-        }
-      });
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - offset
+        }, 1000, function () {// Callback after animation
+          // Must change focus!
+          // var $target = $(target);
+          // $target.focus();
+          // if ($target.is(":focus")) { // Checking if the target was focused
+          //     return false;
+          // } else {
+          //     $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          //     $target.focus(); // Set focus again
+          // }
+        });
+      }
     }
-  }
+  });
 });
