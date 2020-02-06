9f687fdfe9d59216ebf757f5fcdc108bf465a9a6
Eric Greenfield <eric@saltnpixels.com>
2020-02-06 10:35:10 -0500

    changed header block so header tag is insdie conditional

--- template-parts/acf-blocks/header/header.php
+++ template-parts/acf-blocks/header/header.php
@@ -1,19 +1,13 @@
 <?php
-
-/**
- * Header Block Template.
- * The header block is special because we look for this block and if its not found, we output a default header instead.
- *
- * So if you want to add different headers, use this file and the acf header block to make more headers.
- *
- * Add different type of headers by editing this block here and in the ACf Settings
- * Add a new header to the header_type dropdown and make conditional fields show up for the new type of header.
- *
- * @param array $block The block settings and attributes.
- * @param string $content The block inner HTML (empty).
- * @param bool $is_preview True during AJAX preview.
- * @param   (int|string) $post_id The post ID this block is saved to.
- */
+/*
+* Add different type of headers by editing this block here and in the ACF Settings
+* Add a new header to the header_type dropdown and make conditional fields show up for the new type of header.
+*
+* @param array $block The block settings and attributes.
+* @param string $content The block inner HTML (empty).
+* @param bool $is_preview True during AJAX preview.
+* @param   (int|string) $post_id The post ID this block is saved to.
+*/
 
 
 $block_id = isset( $block['anchor'] ) ? $block['anchor'] : 'section-' . $block['id'];
@@ -24,31 +18,34 @@ $header_type = get_field( 'header_type' );
 
 ?>
 
-<header id="<?php echo $block_id; ?>" <?php ign_block_class( $block, 'entry-header layout-center-content ' . $header_type ); ?>
-        <?php if ( $bg_image ){ ?>style="background-image: url('<?php echo $bg_image; ?>');"<?php } ?>>
 
-	<?php if ( $header_type == 'header_paragraphs' ): ?>
-			<div class="columns-holder <?php echo esc_attr( get_field( 'container_class' ) ); ?>">
+<?php if ( $header_type == 'header_paragraphs' ): ?>
 
+	<header id="<?php echo $block_id; ?>" <?php ign_block_class( $block, 'entry-header layout-center-content ' . $header_type ); ?>
+	        <?php if ( $bg_image ){ ?>style="background-image: url('<?php echo $bg_image; ?>');"<?php } ?>>
+		<div class="columns-holder <?php echo esc_attr( get_field( 'container_class' ) ); ?>">
+
+			<?php
+			if ( have_rows( 'paragraphs' ) ) { ?>
+				<div class="<?php echo esc_attr( get_field( 'grid_class' ) ); ?> paragraphs-holder">
+					<?php while ( have_rows( 'paragraphs' ) ): the_row(); ?>
+
+						<div class="text-section <?php the_sub_field( 'paragraph_class' ); ?>">
+							<?php the_sub_field( 'paragraph' ); ?>
+						</div>
+
+					<?php endwhile; ?>
+				</div>
 				<?php
-				if ( have_rows( 'paragraphs' ) ) { ?>
-					<div class="<?php echo esc_attr( get_field( 'grid_class' ) ); ?> paragraphs-holder">
-						<?php while ( have_rows( 'paragraphs' ) ): the_row(); ?>
+			}
+			?>
+		</div>
+	</header>
 
-							<div class="text-section <?php the_sub_field( 'paragraph_class' ); ?>">
-								<?php the_sub_field( 'paragraph' ); ?>
-							</div>
+<?php endif; ?>
 
-						<?php endwhile; ?>
-					</div>
-					<?php
-				}
-				?>
-			</div>
 
-	<?php endif; ?>
-
-	<?php //YOUR NEW HEADER TYPE HERE?>
-
-</header>
-
+<?php //YOUR NEW HEADER TYPE HERE?>
+<?php if ( $header_type == 'something' ): ?>
+	<header></header>
+<?php endif; ?>
