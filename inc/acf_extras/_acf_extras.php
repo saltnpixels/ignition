24dab01ac674b819bc299b4bef3fea9cb44d8606
Eric Greenfield <eric@saltnpixels.com>
2020-04-03 15:55:17 -0400

    Moving theme settings to the top

--- inc/acf_extras/_acf_extras.php
+++ inc/acf_extras/_acf_extras.php
@@ -28,6 +28,7 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		'menu_slug'  => 'theme-general-settings',
 		'capability' => 'edit_posts',
 		'redirect'   => false,
+		'position' => '4.1'
 	) );
 
 
