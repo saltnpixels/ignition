5f971412428cf02f4b1cb5af6ce7b35977b6aa96
Eric Greenfield <eric@saltnpixels.com>
2020-06-30 18:26:43 -0400

    added image assets to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const postcssPresetEnv = require('postcss-preset-env')
 
 
-
 module.exports = {
    entry: {
       frontEnd: './src/index.js',
@@ -23,14 +22,26 @@ module.exports = {
    module: {
       //runs these and transforms on the code
       rules: [
+         {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     name: '[name].[ext]',
+                     outputPath: 'images',
+                  }
+               },
+            ],
+         },
          { test: /\.svg$/, use: 'svg-inline-loader' },
          { test: /\.(js)$/, use: ['babel-loader', 'import-glob-loader'] }, //turns jsx into js the browser can understand and also allows for es6 to be used
          { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
          {
             test: /\.s[ac]ss$/i, use: [
-                {
-                   loader: MiniCssExtractPlugin.loader,
-                }, 'css-loader',
+               {
+                  loader: MiniCssExtractPlugin.loader,
+               }, 'css-loader',
                {
                   loader: 'postcss-loader', options: {
                      ident: 'postcss',
@@ -39,6 +50,7 @@ module.exports = {
                      ]
                   }
                },
+               { loader: 'resolve-url-loader' },
                {
                   loader: 'sass-loader',
                   options: {
